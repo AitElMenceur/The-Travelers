acfd36f3b4206960e8c7b6a12cb045a24d3faa28
Karrina_msw <moussaoui.karina@gmail.com>
2020-05-15 19:08:18 +0200

    remove anuse import

--- src/main/java/com/gui/ChatMain.java
+++ src/main/java/com/gui/ChatMain.java
@@ -1,13 +1,8 @@
 package com.gui;
 
-import java.net.*;
-import java.io.*;
-import java.util.*;
-
-
-public class ChatMain{
+public class ChatMain {
 	public static void main(String[] args) {
 		new ClientChatGUI().setVisible(true);
-		
+
 	}
 }
