51cdcc5026a8ec0bfcab891f36c134462a376484
kalash94 <elmrabti@et.esiea.fr>
2020-05-28 21:13:23 +0200

    JUnits tests created, needs to be implemented

---
+++ src/test/java/com/XmlTest/XmlTest.java
@@ -0,0 +1,119 @@
+package com.XmlTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class XmlTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitializeXml() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransformerXml() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddFriend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddElement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteFriend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddGroupCodeToUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayHistory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListFriend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListOfGroupsOfAUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListOfUsersInGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInTheList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckingLogins() {
+		fail("Not yet implemented");
+	}
+
+}
