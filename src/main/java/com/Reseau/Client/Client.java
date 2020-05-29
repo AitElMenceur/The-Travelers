c380dcb104e5ff28eb7691740eee60bb94848020
Yihsuanlee1112 <yihsuan.rebecca.lee@gmail.com>
2020-05-29 23:37:33 +0800

    Update Client.java
    Deleted class Globals

--- src/main/java/com/Reseau/Client/Client.java
+++ src/main/java/com/Reseau/Client/Client.java
@@ -293,36 +293,11 @@ public class Client implements Runnable {
         }
     }
 
-    // to delete
-    public static class Globals {
-        public static String UserName = "";
-        public static String Passwd = "";
-        // public static String[] Chatters = {"Rebecca", "Nassim", "Marine"};
-        public static String[] Chatters = {};
-        // public static int totalChatters = 0;
-        // public static String[] Freinds = {"Nassim", "Marine", "Karina", "Ilyes"};
-        // public static int totalFreinds = 4;
-        // public static String[] Users = {"Clara", "Meta", "Stella", "Neils"};
-        // public static int totalUsers = 4;
-        // public static String[] GroupMembers = {"Marine", "Nassim",};
-        // public static int totalgroupMembers = 2;
-        public static String[] GroupCode = { "AA", "BB", "CC" };
-        // public static int totalgroupCode = 3;
-        public static String CurrentGroup;
-    }
-
     /**
      * @param arg[]
      */
     public static void main(String arg[]) {
         int i = 6668;
-        // added by Rebecca
-
-        /*
-         * try { LogInGUI dialog = new LogInGUI();
-         * dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-         * dialog.setVisible(true); } catch (Exception e) { e.printStackTrace(); } //end
-         */
 
         Client client = new Client("localhost", i, "User" + i);
         new Thread(client).start();
