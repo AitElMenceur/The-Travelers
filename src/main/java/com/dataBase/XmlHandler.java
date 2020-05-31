65b2d14b17d748246b8d31c7bb2d0d63cbd7d3cf
AitElMenceur <aitelmenceur@et.esiea.fr>
2020-05-31 18:48:16 +0200

    .

--- src/main/java/com/dataBase/XmlHandler.java
+++ src/main/java/com/dataBase/XmlHandler.java
@@ -598,7 +598,7 @@ public class XmlHandler {
 			if (TempString.equals(UserName)) {
 
 				NodeList groups = user.getElementsByTagName("GroupCodes").item(0).getChildNodes();
-				Node group = null;
+				Element group = null;
 				ListOfGroups = new String[groups.getLength()];
 				for (int j = 0; j < groups.getLength(); j++) {
 					group = (Element) groups.item(j);
